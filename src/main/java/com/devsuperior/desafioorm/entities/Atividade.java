15a125528a067fa47424e78808b27a6f97359c00
Marco Antonio <marcoricardo61b@gmail.com>
2025-12-14 12:02:05 -0300

    Categoria and Atividade relantionship created

---
+++ src/main/java/com/devsuperior/desafioorm/entities/Atividade.java
@@ -0,0 +1,70 @@
+package com.devsuperior.desafioorm.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String descricao;
+    private Double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+
+    public Atividade() {
+    }
+
+    public Atividade(Integer id, String name, String descricao, Double preco) {
+        this.id = id;
+        this.name = name;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
